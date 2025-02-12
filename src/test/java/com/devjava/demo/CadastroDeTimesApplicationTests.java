45b7d4a1181eff2c5e8aab471db0dceb0ec3c8b8
Alan <augustoszimcziki@hotmail.com>
2025-02-12 17:25:06 -0300

    first commit

---
+++ src/test/java/com/devjava/demo/CadastroDeTimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjava.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeTimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
