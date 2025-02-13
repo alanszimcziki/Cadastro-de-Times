954be5af2f8bd616099806bf8b5a02cfbf208b73
Alan <augustoszimcziki@hotmail.com>
2025-02-13 11:47:29 -0300

    feat: criando a classe titulos, e criando relacionamento com o banco de dados

---
+++ src/main/java/com/devjava/demo/Titulos/TitulosModel.java
@@ -0,0 +1,21 @@
+package com.devjava.demo.Titulos;
+
+import com.devjava.demo.Times.TimesModel;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name="tb_titulos")
+public class TitulosModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private int quantidade;
+
+    @ManyToOne
+    @JoinColumn(name="times_id") //todo:foreing key chave estrangeira
+    private List<TimesModel> times;
+
+}
