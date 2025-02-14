7f3f5680839d34858b3ca20707e1f3911fb342f7
Alan <augustoszimcziki@hotmail.com>
2025-02-14 11:03:06 -0300

    feat: Adicionando a dependencia lombok para trabalhar com os construtores e deixar o codigo mais limpo

--- src/main/java/com/devjava/demo/Times/TimesModel.java
+++ src/main/java/com/devjava/demo/Times/TimesModel.java
@@ -2,11 +2,17 @@ package com.devjava.demo.Times;
 
 import com.devjava.demo.Titulos.TitulosModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_time")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class TimesModel {
 
     @Id
@@ -20,63 +26,6 @@ public class TimesModel {
     @OneToMany(mappedBy = "times")
     private List<TitulosModel> titulos;
 
-    public TimesModel() {
-    }
 
-    public TimesModel(long id, String nome, String email, String pais, String liga, int capacidade) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.pais = pais;
-        this.liga = liga;
-        this.capacidade = capacidade;
-    }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
-    public String getLiga() {
-        return liga;
-    }
-
-    public void setLiga(String liga) {
-        this.liga = liga;
-    }
-
-    public int getCapacidade() {
-        return capacidade;
-    }
-
-    public void setCapacidade(int capacidade) {
-        this.capacidade = capacidade;
-    }
 }
--- src/main/java/com/devjava/demo/Titulos/TitulosModel.java
+++ src/main/java/com/devjava/demo/Titulos/TitulosModel.java
@@ -2,11 +2,17 @@ package com.devjava.demo.Titulos;
 
 import com.devjava.demo.Times.TimesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name="tb_titulos")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class TitulosModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
